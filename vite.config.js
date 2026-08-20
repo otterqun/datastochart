139f06aeb475cac8b3e912ef40fbe8780535ac97
rafiqun <muhdrafiqazizibinroslan@gmail.com>
2026-08-20 16:10:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue()],
+  base: 'github.com/otterqun/datastochart', 
 })
